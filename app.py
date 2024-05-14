291966e4673a4c9d11bad9a2d29bfef792b8695f
Ricardo Sueiras <ricardo.sueiras@gmail.com>
2024-05-14 14:20:47 +0100

    fix

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-from flask import Flask, render_template, request, json, flash, redirect, url_for
+from flask import Flask, render_template, request, json, flash, redirect, url_for, make_response
 import string
 import random
 import os
 import psycopg2
-
-url_mapping = {}
-URL_MAPPINGS_FILE = 'url_mappings.json'
-
-def load_url_mappings():
-    print("Loading URL mappings from file:", URL_MAPPINGS_FILE)
-    global url_mapping
-    try:
-        with open(URL_MAPPINGS_FILE, 'r') as f:
-            url_mapping = json.load(f)
-    except FileNotFoundError:
-        pass
+import re
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'your_secret_key')
+url_pattern = r'^https?://(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?://(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}'
+def is_valid_url(url):
+    return bool(re.match(url_pattern, url))
 
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
@@ -26,7 +18,7 @@ conn = psycopg2.connect(
     user=os.environ.get('DB_USER'),
     password=os.environ.get('DB_PASSWORD')
 )
-
+        
 # Create a table to store the shortcuts if it doesn't exist
 cur = conn.cursor()
 cur.execute("""
@@ -44,39 +36,37 @@ cur.close()
 def add_url():
     if request.method == 'POST':
         url = request.form['url']
-        if not url:
-            flash('Please enter a valid URL', 'error')
-            return redirect(url_for('add_url'))
-
-        cur = conn.cursor()
-        cur.execute("SELECT COUNT(*) FROM shortcuts WHERE url = %s", (url,))
-        count = cur.fetchone()[0]
-        cur.close()
-
-        if count > 0:
-            flash('The URL already exists', 'error')
-            return redirect(url_for('add_url'))
-
-        # Generate a unique shortcut
-        shortcut = generate_short_id()
-        while True:
+        if is_valid_url(url):
             cur = conn.cursor()
-            cur.execute("SELECT COUNT(*) FROM shortcuts WHERE shortcut = %s", (shortcut,))
+            cur.execute("SELECT COUNT(*) FROM shortcuts WHERE url = %s", (url,))
             count = cur.fetchone()[0]
             cur.close()
-            if count == 0:
-                break
+
+            if count > 0:
+                flash('The URL already exists', 'error')
+                return redirect(url_for('add_url'))
+
+            # Generate a unique shortcut
             shortcut = generate_short_id()
+            while True:
+                cur = conn.cursor()
+                cur.execute("SELECT COUNT(*) FROM shortcuts WHERE shortcut = %s", (shortcut,))
+                count = cur.fetchone()[0]
+                cur.close()
+                if count == 0:
+                    break
+                shortcut = generate_short_id()
 
-        # Save the shortcut and URL to the database
-        cur = conn.cursor()
-        cur.execute("INSERT INTO shortcuts (shortcut, url) VALUES (%s, %s)", (shortcut, url))
-        conn.commit()
-        cur.close()
+            # Save the shortcut and URL to the database
+            cur = conn.cursor()
+            cur.execute("INSERT INTO shortcuts (shortcut, url) VALUES (%s, %s)", (shortcut, url))
+            conn.commit()
+            cur.close()
 
-        flash('Shortcut created', 'success')
-        return redirect(url_for('add_url'))
-
+            flash('Shortcut created', 'success')
+            return redirect(url_for('add_url'))
+        else:
+            return "Invalid URL", 400
     return render_template('add.html')
 
 # Add a route that displays an about page that explains what the app does
@@ -86,11 +76,16 @@ def about():
 
 @app.route('/<short_id>')
 def redirect_url(short_id):
-    if short_id in url_mapping:
-        url = url_mapping[short_id] 
-        return render_template('view.html', short_id=short_id, url=url)
-    else:
-        return "URL not found", 404
+        cur = conn.cursor()
+        cur.execute("SELECT url FROM shortcuts WHERE shortcut = %s", (short_id,))
+        result = cur.fetchone()
+        cur.close()
+
+        if result:
+            url = result[0]
+            return redirect(url, code=302)
+        else:
+            return "URL not found", 404
 
 @app.route('/shortcuts')
 def display_shortcuts():
@@ -104,5 +99,4 @@ def generate_short_id(num_chars=6):
     return ''.join(random.choices(string.ascii_letters+string.digits,k=num_chars))
 
 if __name__ == '__main__':
-    load_url_mappings()
     app.run(debug=False)
