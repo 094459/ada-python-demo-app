62d6c6baef1b8cf7bd176ac65bc712a19b8dfb36
Workshop Participant <participant@workshops.aws>
2024-05-29 14:07:17 +0000

    amazon q developer inline

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 
 url_mapping = {}
 
+# Create a function that returns a random quote that features Yoda
+def yoda_quote():
+    quotes = [
+        "Do. Or do not. There is no try.",
+        "I find your lack of faith disturbing.",
+        "Do or do not. There is no try.",
+        "I am the very model of a modern Major-General.",
+        "I've got a bad feeling about this.",
+        "Do or do not. There is no try.",
+        "I am the very model of a modern Major-General.",
+        "I've got a bad feeling about this."]
+    return random.choice(quotes)
+
+
+
 @app.route('/add', methods=['GET', 'POST'])
 def add_url():
     if request.method == 'POST':
@@ -23,8 +38,12 @@ def redirect_url(short_id):
     else:
         return "URL not found", 404
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', quote=yoda_quote() )
+
 def generate_short_id(num_chars=6):
     return ''.join(random.choices(string.ascii_letters+string.digits,k=num_chars))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
